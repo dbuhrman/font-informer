ef5d323451d31441403a6e23ffa7e07d524dfe99
Bert Verhelst <verhelstbert@gmail.com>
2017-11-08 10:28:45 +0100

    Add postscript name to output + correctly catch errors during tests

--- index.js
+++ index.js
@@ -6,33 +6,44 @@ var fileNameParser = require('./lib/file-name-parser');
 var path = require('path');
 
 module.exports = function (file, filename) {
-	return new Promise(function (resolve, reject) {
-		// If we're passed a buffer, parse it.
-		if(Buffer.isBuffer(file)) {
-			resolve(file);
-		} else {
-			filename = path.relative(path.dirname(file), file);
-			// Otherwise read the file.
-			fs.readFile(file, function(err, contents) {
-				if(err) return reject();
+  return new Promise(function (resolve, reject) {
+    // If we're passed a buffer, parse it.
+    if (Buffer.isBuffer(file)) {
+      resolve(file);
+    } else {
+      filename = path.relative(path.dirname(file), file);
+      // Otherwise read the file.
+      fs.readFile(file, function (err, contents) {
+        if (err) return reject({
+          error: 'unable to read file',
+          innerException: err,
+          filename: filename
+        });
 
-				resolve(contents);
-			});
-		}
-	}).then(function(contents) {
-		return Promise.any(parsers(contents)).then(function(result) {
-			console.log('FILENAME: ', filename);
-			if(filename) {
-				// Trust fileNameParser weight, format, and style (if they exist in filename) over file parsers
-				var fileNameParserResult = fileNameParser(filename);
-				console.log('FILENAME RESULT:', fileNameParserResult);
-				if(fileNameParserResult) {
-					if(fileNameParserResult.weight) result.weight = fileNameParserResult.weight;
-					if(fileNameParserResult.format) result.format = fileNameParserResult.format;
-					if(fileNameParserResult.style) result.style = fileNameParserResult.style;
-				}
-			}
-			return result;
-		});
-	});
+        resolve(contents);
+      });
+    }
+  }).then(function (contents) {
+    return Promise.any(parsers(contents))
+      .then(function (result) {
+        console.log('FILENAME: ', filename);
+        if (filename) {
+          // Trust fileNameParser weight, format, and style (if they exist in filename) over file parsers
+          var fileNameParserResult = fileNameParser(filename);
+          console.log('FILENAME RESULT:', fileNameParserResult);
+          if (fileNameParserResult) {
+            if (fileNameParserResult.weight) result.weight = fileNameParserResult.weight;
+            if (fileNameParserResult.format) result.format = fileNameParserResult.format;
+            if (fileNameParserResult.style) result.style = fileNameParserResult.style;
+          }
+        }
+        return result;
+      })
+      .catch(function (err) {
+        reject({
+          error: 'Failed to parse font, all parsers returned an error',
+          innerException: err
+        });
+      });
+  });
 };
--- test/test.js
+++ test/test.js
@@ -6,69 +6,101 @@ var fileNameExamples = require('./fileNameExamples');
 var fileNameParser = require('../lib/file-name-parser');
 
 describe('font-informer', function () {
-	it('can parse .ttf files', function (done) {
-		informer(path.join(__dirname, 'fonts/pathFont.ttf')).then(function(result) {
-			assert.deepEqual(result, require('./expected.ttf.json'));
-			done();
-		});
-	});
-	it('can parse .woff files', function (done) {
-		informer(path.join(__dirname, 'fonts/pathFont.woff')).then(function(result) {
-			assert.deepEqual(result, require('./expected.woff.json'));
-			done();
-		});
-	});
-	it('can parse .eot files', function (done) {
-		informer(path.join(__dirname, 'fonts/pathFont.eot')).then(function(result) {
-			assert.deepEqual(result, require('./expected.eot.json'));
-			done();
-		});
-	});
-	it('can parse .svg files', function (done) {
-		informer(path.join(__dirname, 'fonts/pathFont.svg')).then(function(result) {
-			assert.deepEqual(result, require('./expected.svg.json'));
-			done();
-		});
-	});
+  it('can parse .ttf files', function (done) {
+    informer(path.join(__dirname, 'fonts/pathFont.ttf'))
+      .then(function (result) {
+        assert.deepEqual(result, require('./expected.ttf.json'));
+        done();
+      })
+      .catch(function (err) {
+        done(err);
+      });
+  });
+  it('can parse .woff files', function (done) {
+    informer(path.join(__dirname, 'fonts/pathFont.woff'))
+      .then(function (result) {
+        assert.deepEqual(result, require('./expected.woff.json'));
+        done();
+      })
+      .catch(function (err) {
+        done(err);
+      });
+  });
+  it('can parse .eot files', function (done) {
+    informer(path.join(__dirname, 'fonts/pathFont.eot'))
+      .then(function (result) {
+        assert.deepEqual(result, require('./expected.eot.json'));
+        done();
+      })
+      .catch(function (err) {
+        done(err);
+      });
+  });
+  it('can parse .svg files', function (done) {
+    informer(path.join(__dirname, 'fonts/pathFont.svg'))
+      .then(function (result) {
+        assert.deepEqual(result, require('./expected.svg.json'));
+        done();
+      })
+      .catch(function (err) {
+        done(err);
+      });
+  });
 
-	describe('parsing buffers', function() {
-		it('can parse .ttf buffers', function(done) {
-			var file = path.join(__dirname, 'fonts', 'pathFont.ttf');
-			informer(fs.readFileSync(file), 'pathFont.ttf').then(function(result) {
-				assert.deepEqual(result, require('./expected.ttf.json'));
-				done();
-			});
-		});
-		it('can parse .woff buffers', function(done) {
-			var file = path.join(__dirname, 'fonts', 'pathFont.woff');
-			informer(fs.readFileSync(file), 'pathFont.woff').then(function(result) {
-				assert.deepEqual(result, require('./expected.woff.json'));
-				done();
-			});
-		});
-		it('can parse .eot buffers', function(done) {
-			var file = path.join(__dirname, 'fonts', 'pathFont.eot');
-			informer(fs.readFileSync(file), 'pathFont.eot').then(function(result) {
-				assert.deepEqual(result, require('./expected.eot.json'));
-				done();
-			});
-		});
-		it('can parse .svg buffers', function(done) {
-			var file = path.join(__dirname, 'fonts', 'pathFont.svg');
-			informer(fs.readFileSync(file), 'pathFont.svg').then(function(result) {
-				assert.deepEqual(result, require('./expected.svg.json'));
-				done();
-			});
-		});
-	});
+  describe('parsing buffers', function () {
+    it('can parse .ttf buffers', function (done) {
+      var file = path.join(__dirname, 'fonts', 'pathFont.ttf');
+      informer(fs.readFileSync(file), 'pathFont.ttf')
+        .then(function (result) {
+          assert.deepEqual(result, require('./expected.ttf.json'));
+          done();
+        })
+        .catch(function (err) {
+          done(err);
+        });
+    });
+    it('can parse .woff buffers', function (done) {
+      var file = path.join(__dirname, 'fonts', 'pathFont.woff');
+      informer(fs.readFileSync(file), 'pathFont.woff')
+        .then(function (result) {
+          assert.deepEqual(result, require('./expected.woff.json'));
+          done();
+        })
+        .catch(function (err) {
+          done(err);
+        });
+    });
+    it('can parse .eot buffers', function (done) {
+      var file = path.join(__dirname, 'fonts', 'pathFont.eot');
+      informer(fs.readFileSync(file), 'pathFont.eot')
+        .then(function (result) {
+          assert.deepEqual(result, require('./expected.eot.json'));
+          done();
+        })
+        .catch(function (err) {
+          done(err);
+        });
+    });
+    it('can parse .svg buffers', function (done) {
+      var file = path.join(__dirname, 'fonts', 'pathFont.svg');
+      informer(fs.readFileSync(file), 'pathFont.svg')
+        .then(function (result) {
+          assert.deepEqual(result, require('./expected.svg.json'));
+          done();
+        })
+        .catch(function (err) {
+          done(err);
+        });
+    });
+  });
 
-	describe('parsing filenames', function() {
-		it('can parse common filenames', function(done) {
-			fileNameExamples.forEach(function(example) {
-				const result = fileNameParser(example.input);
-				assert.deepEqual(result, example.expected);
-			});
-			done();
-		})
-	})
+  describe('parsing filenames', function () {
+    it('can parse common filenames', function (done) {
+      fileNameExamples.forEach(function (example) {
+        const result = fileNameParser(example.input);
+        assert.deepEqual(result, example.expected);
+      });
+      done();
+    })
+  })
 });
